977876d7ec12569282ae26f7446b37e14f9947cb
sheikhtazuddin <tazahmedsoft@gmail.com>
2024-01-01 13:00:48 +0600

    implement login system

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,21 +1,38 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { createContext } from "react";
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
 import { auth } from "../fireBase/firebase.config";
 
 
-export const AuthContext = createContext()
+export const AuthContext = createContext(null)
 const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState()
+    const [loader, setLoader] = useState(true)
 
-
+    // sign up authenticastion
     const signUp = (email, password) => {
+        setLoader(true)
         return createUserWithEmailAndPassword(auth, email, password)
     }
-
-
-
+const signIn=(email,password)=>{
+    setLoader(false);
+    return signInWithEmailAndPassword(auth,email,password)
+}
+    //get current user
+    useEffect(() => {
+        const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser)
+            setLoader(false)
+            console.log(currentUser)
+        })
+        return () => unSubscribe()
+    }, [])
+    // context value
     const authInFo = {
         auth,
         signUp,
+        user,
+        loader,
+        signIn
     }
 
     return (
--- src/pages/UI/Login/Login.jsx
+++ src/pages/UI/Login/Login.jsx
@@ -1,9 +1,77 @@
-import React from 'react';
+import { useForm } from 'react-hook-form';
+import useAuth from '../../../Hooks/auth/useAuth';
+import Swal from 'sweetalert2';
+
 
 const Login = () => {
+    const { signIn } = useAuth()
+    const {
+        register,
+        handleSubmit,
+        reset,
+    } = useForm()
+
+    //from submit
+    const onSubmit = async (data) => {
+        console.log(data)
+        const userEmail = data?.email;
+        const userPass = data?.password;
+
+        signIn(userEmail, userPass)
+            .then(res => {
+                reset({
+                    email:'',
+                    password:''
+                })
+                // console.log(res)
+                Swal.fire({
+                    position: "center",
+                    icon: "success",
+                    title: "SuccessFully you create a account",
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+            }).catch(error => {
+                const errorMessage = error.message;
+                // console.log(errorMessage)
+                Swal.fire({
+                    position: "center",
+                    icon: "error",
+                    title: `${errorMessage}`,
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+            })
+    }
     return (
         <div>
-            
+            <div className=" flex justify-center my-20">
+                {/* <div className=""> */}
+
+                <div className="card w-full max-w-md shadow-2xl shadow-black border border-yellow-300">
+
+                    <form className="card-body" onSubmit={handleSubmit(onSubmit)}>
+                        <h1 className="text-3xl text-center font-bold">Sign In</h1>
+
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" placeholder="email" className="input input-bordered border-yellow-500" {...register('email')} required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" placeholder="password" className="input input-bordered border-yellow-500" {...register('password')} required />
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary" type="submit">sign in</button>
+                        </div>
+                    </form>
+                </div>
+                {/* </div> */}
+            </div>
         </div>
     );
 };
